747458f6f7b4c3fcdfeaf53050f63106684ca335
yahiaBSK <boussouakyahia@gmail.com>
2024-09-01 22:14:21 +0100

    update studentsStore.jsx

--- src/stores/studentsStore.jsx
+++ src/stores/studentsStore.jsx
@@ -1,28 +1,26 @@
 import { create } from "zustand";
-import Api from "../api/api"
+import Api from "../api/api";
 import axios from "axios";
 
-
 export const studentStore = create((set) => {
-
   return {
-
     addStudent: (students) => set({ students }),
 
-    getStudent: (all_students) => set({
-      getStudentsFromApi: () => {
-        setLoadingData(true);
-        axios.get(`${Api}/students`)
-         .then((response) => {
-            setLoadingData(false);
-            setStudents(response.data);
-          })
-         .catch((error) => {
-            console.log(error);
-            setLoadingData(false);
-          });
-      },
-    })
-
+    getStudent: (all_students) =>
+      set({
+        getStudentsFromApi: () => {
+          setLoadingData(true);
+          axios
+            .get(`${Api}/students`)
+            .then((response) => {
+              setLoadingData(false);
+              setStudents(response.data);
+            })
+            .catch((error) => {
+              console.log(error);
+              setLoadingData(false);
+            });
+        },
+      }),
   };
 });
